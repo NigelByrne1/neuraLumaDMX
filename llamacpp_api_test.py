320455107a7e9bbd23532c5a9da3dfdcc7317505
Nigel <nigelbyrnedesign@gmail.com>
2026-03-06 15:03:21 +0000

    Local LLM responding with colour values from natural language

--- llamacpp_api_test.py
+++ llamacpp_api_test.py
@@ -4,9 +4,13 @@ url = "http://127.0.0.1:8033/v1/chat/completions"
 
 data = {
     "messages": [
+        {  
+            "role": "system", 
+            "content": " YOU ARE A DMX CONTROLLER ONLY REPLY WITH THE CORRECT COLOUR RGBW VALUES FOR THE REQUESTED COLOUR, E.G. USER ASKS FOR RED YOUR RESPONSE IS 255,0,0,0 OR USER ASKS FOR BLUE YOUR RESPONSE IS 0,255,0,0"
+        },
         {
             "role": "user", 
-            "content": "What is the capital of Ireland? just respond with the name"
+            "content": "MAKE THE COLOUR ORANGE"
         }
     ]
 }
